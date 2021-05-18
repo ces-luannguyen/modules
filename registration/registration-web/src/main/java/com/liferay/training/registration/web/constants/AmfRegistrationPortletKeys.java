1d0b5d0c30632d5693e585cacf39fc9b146f28a6
Luan Nguyen <luan.nguyen@codeenginestudio.com>
2021-05-18 08:01:28 +0700

    Create the registration web module

---
+++ registration/registration-web/src/main/java/com/liferay/training/registration/web/constants/AmfRegistrationPortletKeys.java
@@ -0,0 +1,11 @@
+package com.liferay.training.registration.web.constants;
+
+/**
+ * @author ces-user
+ */
+public class AmfRegistrationPortletKeys {
+
+	public static final String AMFREGISTRATION =
+		"com_liferay_training_registration_web_AmfRegistrationPortlet";
+
+}
